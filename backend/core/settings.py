e8e3f4c8ada019a823cef72b8663d4e1361ea6b7
repsandcode <albertjonathan.dev@gmail.com>
2024-01-04 21:26:59 +0800

    hide env variables

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import Config, Csv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0d)c-)+*@$+y576=!0v^3yu=6c+@3ed03!+wa*0w+lz)stwoz3'
+config = Config()
+config.read_env()
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
